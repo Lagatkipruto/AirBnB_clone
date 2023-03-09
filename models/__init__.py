707e9b9885337b50d9f1edec8a3cda5e35e1f2ce
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-09 06:50:25 -0800

    updating init

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This is the initialization file"""
 from models.engine.file_storage import FileStorage
+"""updating to create variable and call storage"""
 
 
 storage = FileStorage()
